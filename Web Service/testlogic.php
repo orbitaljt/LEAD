e777d9be1e075f018de84872094400eaec97ab29
wheel1992 <the_only_light@hotmail.com>
2015-06-21 08:52:24 +0800

    Removed

--- Web Service/testlogic.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	require_once('logic/logic.php');
-
-	$output = Logic::getUserProfile('14200848000001729423455');
-	echo $output;
-	echo "<br/>";
-
-?>
--- Web Service/testrandomid.php
+++
@@ -1,6 +0,0 @@
-<?php
-	
-	echo round(microtime(true) * 1000) ."<br/>";
-	echo mt_rand();
-
-?>
